d25c40fbd08a80f5afede0138a956c3b80c28638
jakea1998 <77550794+jakea1998@users.noreply.github.com>
2021-11-06 16:10:55 -0500

    integrated firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:bunkie/screens/home.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+void main() async{
+   WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp();
+  
+  await Firebase.initializeApp();
+  
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+  
+  
   runApp(MyApp());
 }
 
